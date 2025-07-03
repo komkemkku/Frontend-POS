1c82676cdb028c3de29869adb99654113ec94916
komkem <komkem.k@kkumail.com>
2025-07-04 03:49:31 +0700

    add : api railway not sure can use it

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://172.20.10.3:8080'
+    baseURL: 'http://backend-pos-production.up.railway.app:8080'
 });
 
 // เพิ่ม Token ในทุก Request โดยอัตโนมัติ
